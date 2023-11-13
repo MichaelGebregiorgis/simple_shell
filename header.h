e89f369ab6718e86d546f65399ff418f854ddb6e
Michael Gebregiorgis <you@example.com>
2023-11-13 05:00:42 -0800

    header file

---
+++ header.h
@@ -0,0 +1,11 @@
+#ifndef header_h
+#define header_h
+
+void hndlExit();
+void hndlExit();
+void excCmd(const char *cmd);
+void snline(char *str);
+void chngeDir(const char *dir);
+void hndlPwd();
+
+#endif
